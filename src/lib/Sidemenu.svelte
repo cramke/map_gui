996529e63bc5207492674bab92f911893b867b58
cramke <carsten.ramke@gmail.com>
2024-02-24 17:14:30 +0100

    Sidemenu style

--- src/lib/Sidemenu.svelte
+++ src/lib/Sidemenu.svelte
@@ -4,36 +4,23 @@
 </script>
 
 <!-- sidemenu.html -->
-<nav class="sidemenu">
-    <ul>
+<nav>
+    <ul class="sidemenu">
         <li><a href="/">Home</a></li>
         <li><a href="/map">Map</a></li>
         <li><a href="/settings">Settings</a></li>
     </ul>
-
     <div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-    <div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+        <a href="https://github.com/sveltejs/kit">
+            <img src={github} alt="GitHub" />
+        </a>
+    </div>
 </nav>
 
 <style>
     .sidemenu {
-        height: 100%;
         background-color: #e5f5df;
         padding: 20px;
-    }
-    
-    .sidemenu ul {
-        list-style: none;
-        padding: 0;
-        margin: 0;
     }
 
     .sidemenu li {
@@ -53,19 +40,6 @@
         padding: 10px;
         border-radius: 5px;
     }
-
-    .corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
 
 	.corner img {
 		width: 2em;
